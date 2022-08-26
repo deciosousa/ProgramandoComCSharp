f238937b94250fa2731b1d7cdbefba6436c32fc7
DECIO SOUSA <deciojsfilho@gmail.com>
2022-08-26 15:55:10 -0300

    Adicionar arquivos de projeto.

---
+++ CSharp.Capitulo01.Sintaxe/VariaveisForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace CSharp.Capitulo01.Sintaxe
+{
+    partial class VariaveisForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.operaçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aritmeticasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resultadoListBox = new System.Windows.Forms.ListBox();
+            this.reduzidasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.operaçõesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // operaçõesToolStripMenuItem
+            // 
+            this.operaçõesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aritmeticasToolStripMenuItem,
+            this.reduzidasToolStripMenuItem});
+            this.operaçõesToolStripMenuItem.Name = "operaçõesToolStripMenuItem";
+            this.operaçõesToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.operaçõesToolStripMenuItem.Text = "Operações";
+            // 
+            // aritmeticasToolStripMenuItem
+            // 
+            this.aritmeticasToolStripMenuItem.Name = "aritmeticasToolStripMenuItem";
+            this.aritmeticasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aritmeticasToolStripMenuItem.Text = "Aritméticas";
+            this.aritmeticasToolStripMenuItem.Click += new System.EventHandler(this.aritmeticasToolStripMenuItem_Click);
+            // 
+            // resultadoListBox
+            // 
+            this.resultadoListBox.FormattingEnabled = true;
+            this.resultadoListBox.Location = new System.Drawing.Point(0, 27);
+            this.resultadoListBox.Name = "resultadoListBox";
+            this.resultadoListBox.Size = new System.Drawing.Size(800, 407);
+            this.resultadoListBox.TabIndex = 1;
+            // 
+            // reduzidasToolStripMenuItem
+            // 
+            this.reduzidasToolStripMenuItem.Name = "reduzidasToolStripMenuItem";
+            this.reduzidasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.reduzidasToolStripMenuItem.Text = "Reduzidas";
+            this.reduzidasToolStripMenuItem.Click += new System.EventHandler(this.reduzidasToolStripMenuItem_Click);
+            // 
+            // VariaveisForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultadoListBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "VariaveisForm";
+            this.Text = "Variaveis e Operações";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem operaçõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aritmeticasToolStripMenuItem;
+        private System.Windows.Forms.ListBox resultadoListBox;
+        private System.Windows.Forms.ToolStripMenuItem reduzidasToolStripMenuItem;
+    }
+}
+
---
+++ CSharp.Capitulo01.Sintaxe/VariaveisForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CSharp.Capitulo01.Sintaxe
+{
+    public partial class VariaveisForm : Form
+    {
+        public VariaveisForm()
+        {
+            InitializeComponent();
+        }
+
+        private void aritmeticasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // -------- CRIAÇÃO DE VARIÁVEIS ------
+
+            int x = 42;
+            int X = 10;
+
+            string nome = "Décio";
+            char letra = 'a';
+            bool aprovado = true;
+            aprovado = false; // não é necessário criar a variável novamente para utilizá-la.
+
+            DateTime dataNascimento = new DateTime(2000,1,1);
+
+            var a = 2; // a palavra reservada 'var' usa a inferência de tipo
+            var b = 6;
+            var c = 10;
+            var d = 13;
+
+            // 'var' não pode ser usado sem o conceito de inferência, ou seja, sem o sinal de atribuição (=)
+
+            object meuObjeto = 35; // o 'object' é flexível em relação aos tipos de dados que podem ser usados.
+            meuObjeto = "Décio";
+            meuObjeto = false;
+
+            // -------- FRACIONÁRIOS ------
+
+            var valor = 7.8m; // a letra 'm' é usada para criar variáveis do tipo decimal.
+
+            // o 'float' e o 'double' não se indicados para trabalhar com valores monetários, por não realizarem o arredondamento adequadamente. Para valores decimais é recomendado o uso de 'decimal'.
+
+            var bimestre1 = 5.9m;
+
+            // -------- INICIALIZAR MÚLTIPLAS VARIÁVEIS ------
+            int numero = 11, outroNumero = 12, esseNumero = 14; // é possível inicializar múltiplas variáveis na mesma linha, desde que todas sejam do mesmo tipo. Esta não é uma boa prática.
+
+            // -------- CONCATENAR ------
+            resultadoListBox.Items.Add("a="+a); // 'items' é característica e 'Add' é método (ação / verbo no infinitivo).
+            resultadoListBox.Items.Add(string.Concat("b=",b)); 
+            resultadoListBox.Items.Add(string.Format("c={0}",c)); 
+            //resultadoListBox.Items.Add(string.Format("c={0}-d={1}",c,d));
+            resultadoListBox.Items.Add($"d={d}");
+
+            // -------- OPERAÇÕES ARITMÉTICAS ------
+
+            resultadoListBox.Items.Add($"c*d={c*d}");
+            resultadoListBox.Items.Add($"d/a={d/a}");
+            resultadoListBox.Items.Add($"d%a={d%a}");
+        }
+
+        private void reduzidasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // -------- OPERAÇÕES ARITMÉTICAS COM NOTAÇÃO REDUZIDA ------
+            var x = 5;
+            resultadoListBox.Items.Add("x=" + x);
+
+            x = x - 3;
+            x -= 3; // essa é a escrita abreviada/reduzida. Vbale para outras operações aritméticas.
+            resultadoListBox.Items.Add("x=" + x);
+        }
+    }
+}
